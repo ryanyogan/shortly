c4a6d8ee72bd66383e34873e1a900390b3942a66
Ryan Yogan <ryanyogan@gmail.com>
2018-01-21 12:43:13 -0800

    Added in GC serverless function for hashing invocation on sub mutation_in: CREATED

---
+++ graphcool/src/createShortLink.js
@@ -0,0 +1,60 @@
+const { fromEvent } = require('graphcool-lib');
+
+const createHash = itemCount => {
+  const hashDigits = [];
+  // dividend is a unique integer (in our case, number of links)
+  let dividend = itemCount + 1;
+  let remainder = 0;
+  while (dividend > 0) {
+    remainder = dividend % 62;
+    dividend = Math.floor(dividend / 62);
+    hashDigits.unshift(remainder);
+  }
+  const alphabetArray = `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789`.split(
+    ''
+  );
+  // Convert hashDigits to base62 representation
+  let hashString = '';
+  let i = 0;
+  while (hashDigits.length > i) {
+    hashString += alphabetArray[hashDigits[i]];
+    i++;
+  }
+  return hashString;
+};
+
+module.exports = async event => {
+  const { id } = event.data.Link.node;
+  const graphcool = fromEvent(event);
+  const api = graphcool.api('simple/v1');
+
+  const getLinkCountQuery = `
+    query GetLinkCountQuery {
+      links: _allLinksMeta {
+        count
+      }
+    }
+  `;
+
+  const linkCountQueryResult = await api.request(getLinkCountQuery);
+  const linkCount = linkCountQueryResult.links.count;
+
+  const hash = createHash(linkCount);
+
+  const updateLinkMutation = `
+    mutation ($id: ID!, $hash: String!) {
+      updateLink(id: $id, hash: $hash) {
+        id
+      }
+    }
+  `;
+
+  const variables = { id, hash };
+  await api.request(updateLinkMutation, variables);
+
+  return {
+    data: {
+      success: true
+    }
+  };
+};
